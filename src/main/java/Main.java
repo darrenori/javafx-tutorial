d3e467e5fe1e7304401dbf1c86779146c915f911
darrenori <darrennorii@gmail.com>
2026-08-27 17:26:21 +0800

    Feat: Show Duke's face as the window icon
    The window carried the stock Java icon in the taskbar, which says nothing
    about the app. Reuse the avatar that already ships in the resources rather
    than adding another image.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -21,6 +22,7 @@ public class Main extends Application {
             Scene scene = new Scene(ap);
             stage.setScene(scene);
             stage.setTitle("Duke");
+            stage.getIcons().add(new Image(Main.class.getResourceAsStream("/images/DaDuke.png")));
             stage.setMinHeight(220.0);
             stage.setMinWidth(417.0);
             fxmlLoader.<MainWindow>getController().setDuke(duke); // inject the Duke instance
